faa30c8c2bcb51d96d579809d5552b7ab9067c0e
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-02-27 18:35:13 +0700

    feat: add database for table users with persistence room

---
+++ app/src/main/java/com/phunlh2001/fooddelivery/data/db/UserDatabase.java
@@ -0,0 +1,27 @@
+package com.phunlh2001.fooddelivery.data.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import com.phunlh2001.fooddelivery.data.dao.UserDAO;
+import com.phunlh2001.fooddelivery.data.model.User;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class UserDatabase extends RoomDatabase {
+    private static UserDatabase INSTANCE;
+
+    public abstract UserDAO getUserDAO();
+    public static UserDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.inMemoryDatabaseBuilder(context.getApplicationContext(), UserDatabase.class)
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
