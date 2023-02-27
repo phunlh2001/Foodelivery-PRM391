d75eb4f0c43245d0adc8d4bfc3339c69fc1d4a53
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-02-27 18:35:25 +0700

    feat: add database for table products with persistence room

---
+++ app/src/main/java/com/phunlh2001/fooddelivery/data/db/ProductDatabase.java
@@ -0,0 +1,27 @@
+package com.phunlh2001.fooddelivery.data.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import com.phunlh2001.fooddelivery.data.dao.ProductDAO;
+import com.phunlh2001.fooddelivery.data.model.Product;
+
+@Database(entities = {Product.class}, version = 1)
+public abstract class ProductDatabase extends RoomDatabase {
+    private static ProductDatabase INSTANCE;
+
+    public abstract ProductDAO getProductDAO();
+    public static ProductDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.inMemoryDatabaseBuilder(context.getApplicationContext(), ProductDatabase.class)
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
